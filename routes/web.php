f4005fe4d8e8a35fcb69de49d8fbadecad04d677
Studentalthaf <09010621013@student.uinsby.ac.id>
2025-08-27 19:28:30 +0700

    7

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\BarangController;
 use App\Models\Barang;
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\ProfileController;
 
 
 Route::get('/', function () {
@@ -17,5 +18,14 @@ Route::post('/barang', [BarangController::class, 'store'])->name('barang.store.m
 // Route::resource('barang', BarangController::class); // Jika ada, komen dulu
 
 Route::get('/landing', [LandingPageController::class, 'index'])->name('landing');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route dashboard jika ingin terpisah dari /
+    Route::get('/dashboard', function () {
+        $barang = \App\Models\Barang::all();
+        return inertia('Dashboard', ['barang' => $barang]);
+    })->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
